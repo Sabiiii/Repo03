2f2a26f642e7ff8e1fcb664a122188c29d70aa3c
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-15 23:23:27 +0400

    Adding case sensitivity

---
+++ ex07/src/index.js
@@ -0,0 +1,33 @@
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+function main() {
+
+    var studlyCapVar = 10;
+    var properCamelCase = "A String";
+    var titleCaseOver = 9000;
+
+
+
+    return {
+
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+
+    }
+
+}
+
+console.log(main());
+
+module.exports = main;
+
