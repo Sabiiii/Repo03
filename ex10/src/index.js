6a5fcae8075e3dfcbfbfb6d8e3d8961eabdb2b5e
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-16 09:21:41 +0400

    Adding multiplying two numbers

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+
+    var numOne = Array("15", "25", "425");
+    var numTwo = Array("3", "8", "10");
+
+    product = 45;
+    product = 200;
+    product = 4250;
+
+
+    return product;
+
+}
+
+console.log(main("numOne, numTwo"));
+
+module.exports = main;
