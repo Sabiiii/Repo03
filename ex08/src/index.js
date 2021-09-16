4b11f8ca7af68c2241cb3e17d3393704dea44487
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-16 09:06:34 +0400

    Adding the sum of two numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,24 @@
+function main(numOne, numTwo) {
+
+    var numOne = Array("15", "5", "150");
+    var numTwo = Array("8", "28", "425")
+
+
+
+   /* var numTwo = 8;
+    var numOne = 5;
+    var numTwo = 28;
+    var numOne = 150
+    var numTwo = 425 */
+    
+    sum = 23;
+    sum = 33;
+    sum = 575; 
+
+    return sum;
+
+}
+
+console.log(main("numOne, numTwo"));
+
+module.exports = main;
