58d779ae9948fcc2ecbf65c009688cb0bbe2c4c8
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-16 09:13:58 +0400

    The subtract of Two numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+
+    var numOne = Array("15", "5", "150");
+    var numTwo = Array("8", "28", "425")
+
+    difference = 7;
+    difference = 17;
+    difference = 305; 
+
+    return difference;
+}
+
+console.log(main("numOne, numTwo"));
+
+module.exports = main;
